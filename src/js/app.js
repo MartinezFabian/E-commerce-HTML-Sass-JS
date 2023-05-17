61e89e357a9976bedbf0396d2ab437507e97e91d
MartinezFabian <FabianMartinezDev@gmail.com>
2023-05-17 19:39:29 -0300

    feat: Add functionality to save cart products to local storage

--- src/js/app.js
+++ src/js/app.js
@@ -1,103 +1,120 @@
-// variables
-const cart = document.querySelector(".cart");
-const cartProducts = document.querySelector(".cart__products");
-const cartIcon = document.querySelector(".header__logo-icon");
-const productsGrid = document.querySelector(".products__grid");
-const btnClearCart = document.querySelector("#clear-cart");
-const productCounter = document.querySelector("#product-counter");
-const productCounterContainer = document.querySelector(".cart__count");
-let products = [];
+document.addEventListener("DOMContentLoaded", () => {
+  main();
+});
 
-//functions
+function main() {
+  // variables
+  const cart = document.querySelector(".cart");
+  const cartProducts = document.querySelector(".cart__products");
+  const cartIcon = document.querySelector(".header__logo-icon");
+  const productsGrid = document.querySelector(".products__grid");
+  const btnClearCart = document.querySelector("#clear-cart");
+  const productCounter = document.querySelector("#product-counter");
+  const productCounterContainer = document.querySelector(".cart__count");
+  let products = [];
 
-registerEventListeners();
+  //functions
 
-function registerEventListeners() {
-  cartIcon.addEventListener("click", toggleCart);
-  productsGrid.addEventListener("click", addProduct);
-  cartProducts.addEventListener("click", deleteProduct);
-  btnClearCart.addEventListener("click", () => {
-    products = []; //vaciar array
-    clearCartProductsHTML(); //limpiar HTML del carrito
-    updateProductCounter(); // Actualiza el contador de productos
-  });
-}
+  //traemos los datos guardados en local storage
+  getDataLocalStorage();
 
-function toggleCart() {
-  //.toggle() agrega o quita la clase del elemento dependiendo de si ya contiene la clase o no
-  cart.classList.toggle("cart--hidden");
-}
+  function getDataLocalStorage() {
+    // Verificar si hay datos almacenados en el local storage con la clave products
+    if (localStorage.getItem("products")) {
+      // Obtener los datos almacenados y convertirlos de nuevo a un array
+      products = JSON.parse(localStorage.getItem("products"));
 
-function addProduct(e) {
-  // Verifica si el elemento que ha desencadenado el evento tiene la clase "add-to-cart"
-  if (e.target.classList.contains("add-to-cart")) {
-    //obtenemos el elemento HTML que contiene todo el producto
-    const productHTML = e.target.parentElement.parentElement;
-    readProductData(productHTML);
+      // actualizar el contenido HTML con los datos obtenidos
+      updateCartHTML();
+
+      //Actualizar el contador del carrito en base a los datos obtenidos
+      updateProductCounter();
+    }
   }
-}
 
-function deleteProduct(e) {
-  // Verifica si el elemento que ha desencadenado el evento tiene la clase "cart__delete-icon"
-  if (e.target.classList.contains("cart__delete-icon")) {
-    // Obtiene el valor del atributo 'data-id' del elemento que se debe eliminar
-    const idProductToBeRemoved = e.target.getAttribute("data-id");
+  registerEventListeners();
 
-    // obtener un nuevo array sin el producto que tiene el mismo 'id' que el elemento a eliminar
-    products = products.filter(
-      (product) => product.id !== idProductToBeRemoved
-    );
+  function registerEventListeners() {
+    cartIcon.addEventListener("click", toggleCart);
+    productsGrid.addEventListener("click", addProduct);
+    cartProducts.addEventListener("click", deleteProduct);
+    btnClearCart.addEventListener("click", () => {
+      products = []; //vaciar array
+      clearCartProductsHTML(); //limpiar HTML del carrito
+      updateProductCounter(); // Actualiza el contador de productos
+      syncLocalStorage(); //Limpia el array en local storage
+    });
+  }
 
-    // Actualiza la representación visual del carrito de compras
+  function toggleCart() {
+    //.toggle() agrega o quita la clase del elemento dependiendo de si ya contiene la clase o no
+    cart.classList.toggle("cart--hidden");
+  }
+
+  function addProduct(e) {
+    // Verifica si el elemento que ha desencadenado el evento tiene la clase "add-to-cart"
+    if (e.target.classList.contains("add-to-cart")) {
+      //obtenemos el elemento HTML que contiene todo el producto
+      const productHTML = e.target.parentElement.parentElement;
+      readProductData(productHTML);
+    }
+  }
+
+  function deleteProduct(e) {
+    // Verifica si el elemento que ha desencadenado el evento tiene la clase "cart__delete-icon"
+    if (e.target.classList.contains("cart__delete-icon")) {
+      // Obtiene el valor del atributo 'data-id' del elemento que se debe eliminar
+      const idProductToBeRemoved = e.target.getAttribute("data-id");
+
+      // obtener un nuevo array sin el producto que tiene el mismo 'id' que el elemento a eliminar
+      products = products.filter((product) => product.id !== idProductToBeRemoved);
+
+      // Actualiza la representación visual del carrito de compras
+      updateCartHTML();
+      // Actualiza el contador de productos
+      updateProductCounter();
+    }
+  }
+
+  function readProductData(productHTML) {
+    //crear un objeto con los datos del producto seleccionado
+    const productInfo = {
+      id: productHTML.querySelector(".add-to-cart").getAttribute("data-id"),
+      name: productHTML.querySelector(".product__name").textContent,
+      price: productHTML.querySelector(".product__price").textContent,
+      image: productHTML.querySelector(".product__image").src,
+      quantity: 1,
+    };
+
+    //Verificar si el producto ya existe en el array products
+    const productExist = products.some((product) => product.id === productInfo.id);
+
+    if (productExist) {
+      //obtener el indice del producto que ya existe para actualizar la cantidad
+      const index = products.findIndex((product) => product.id === productInfo.id);
+
+      products[index].quantity++;
+    } else {
+      //Agregar el objeto producto al arreglo products
+      products.push(productInfo);
+      updateProductCounter();
+    }
+
     updateCartHTML();
-    // Actualiza el contador de productos
-    updateProductCounter();
-  }
-}
-
-function readProductData(productHTML) {
-  //crear un objeto con los datos del producto seleccionado
-  const productInfo = {
-    id: productHTML.querySelector(".add-to-cart").getAttribute("data-id"),
-    name: productHTML.querySelector(".product__name").textContent,
-    price: productHTML.querySelector(".product__price").textContent,
-    image: productHTML.querySelector(".product__image").src,
-    quantity: 1,
-  };
-
-  //Verificar si el producto ya existe en el array products
-  const productExist = products.some(
-    (product) => product.id === productInfo.id
-  );
-
-  if (productExist) {
-    //obtener el indice del producto que ya existe para actualizar la cantidad
-    const index = products.findIndex(
-      (product) => product.id === productInfo.id
-    );
-
-    products[index].quantity++;
-  } else {
-    //Agregar el objeto producto al arreglo products
-    products.push(productInfo);
-    updateProductCounter();
   }
 
-  updateCartHTML();
-}
+  function updateCartHTML() {
+    //Limpiar el HTML del carrito
+    clearCartProductsHTML();
 
-function updateCartHTML() {
-  //Limpiar el HTML del carrito
-  clearCartProductsHTML();
+    //Generar HTML para cada product del array y agregarlo al carrito
+    products.forEach((product) => {
+      const { image, name, price, id, quantity } = product;
+      const cardProduct = document.createElement("div");
 
-  //Generar HTML para cada product del array y agregarlo al carrito
-  products.forEach((product) => {
-    const { image, name, price, id, quantity } = product;
-    const cardProduct = document.createElement("div");
-
-    cartProducts.insertAdjacentHTML(
-      "beforeend",
-      `
+      cartProducts.insertAdjacentHTML(
+        "beforeend",
+        `
         <div class="cart__product">
         <div class="cart__info">
           <img src=${image} class="cart__info-image">
@@ -125,25 +142,34 @@ function updateCartHTML() {
         </div>
       </div>
       `
-    );
-  });
-}
+      );
+    });
 
-//eliminar todos los elementos hijos del contenedor cartProducts
-function clearCartProductsHTML() {
-  // Mientras exista un primer hijo en el contenedor cartProduct
-  while (cartProducts.firstChild) {
-    // Remover el primer hijo del contenedor cartProducts
-    cartProducts.removeChild(cartProducts.firstChild);
+    //Agrega los productos actuales a local storage
+    syncLocalStorage();
   }
-}
 
-function updateProductCounter() {
-  productCounter.textContent = products.length;
+  //eliminar todos los elementos hijos del contenedor cartProducts
+  function clearCartProductsHTML() {
+    // Mientras exista un primer hijo en el contenedor cartProduct
+    while (cartProducts.firstChild) {
+      // Remover el primer hijo del contenedor cartProducts
+      cartProducts.removeChild(cartProducts.firstChild);
+    }
+  }
 
-  if (products.length > 0) {
-    productCounterContainer.classList.add("cart__count--red");
-  } else {
-    productCounterContainer.classList.remove("cart__count--red");
+  function updateProductCounter() {
+    productCounter.textContent = products.length;
+
+    if (products.length > 0) {
+      productCounterContainer.classList.add("cart__count--red");
+    } else {
+      productCounterContainer.classList.remove("cart__count--red");
+    }
+  }
+
+  //Guarda el array products en local storage
+  function syncLocalStorage() {
+    localStorage.setItem("products", JSON.stringify(products));
   }
 }
